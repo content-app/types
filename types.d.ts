de592a3d5a5efa7f3bb5ac956385d24c5ef2c59c
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2023-04-10 08:12:09 +0000

    0.1.0

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,20 @@
 import { ClientAPI, Environment, Space } from 'contentful-management';
+import { ContentfulClientApi } from 'contentful';
 
 export interface ModuleInstallProps {
     client: ClientAPI;
     environment: Environment;
     space: Space;
-}
+};
+
+
+interface ContentModuleConfig {
+    client: ContentfulClientApi;
+    moduleData: any; 
+    fetchFromContentful: () => Promise<any>;
+};
+
+export interface ContentModule {
+    name: string;
+    fetch(data: ContentModuleConfig): Promise<any>;
+};
